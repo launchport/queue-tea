8ebd92a40ec00bdab42d624f9db239e2359e19ce
Daniel Lehr <daniellehr@gmx.de>
2021-07-16 15:27:21 +0200

    more setup

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  preset: 'ts-jest/presets/js-with-babel',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['node_modules/?!(nanoevents)/'],
-}
