36d7a89eb5c60e1ba3069dbfaea8c3b432964afb
Daniel Lehr <daniellehr@gmx.de>
2021-07-16 11:56:20 +0200

    setup

---
+++ src/TaskQueue.spec.ts
@@ -0,0 +1,93 @@
+import waitForExpect from 'wait-for-expect'
+import { TaskQueue } from './TaskQueue'
+
+const flushPromises = () => new Promise((resolve) => setImmediate(resolve))
+
+describe('TaskQueue', () => {
+  it.todo('should execute one task at a time')
+
+  it('should be able to run and pause the queue', async () => {
+    const mocka = jest.fn()
+    const queue = TaskQueue<{ run: undefined }>({ tasks: { run: mocka } })
+
+    jest.useFakeTimers()
+    queue.queueTask('run')
+    queue.queueTask('run')
+    queue.queueTask('run')
+
+    queue.run()
+    queue.pause()
+
+    await waitForExpect(() => {
+      expect(queue.getQueueItems().length).toBe(2)
+      expect(mocka).toHaveBeenCalledTimes(1)
+    })
+
+    jest.runAllTicks()
+    expect(mocka).not.toHaveBeenCalledTimes(2)
+
+    queue.run()
+
+    await waitForExpect(() => {
+      expect(queue.getQueueItems().length).toBe(0)
+      expect(mocka).toHaveBeenCalledTimes(3)
+    })
+
+    jest.useRealTimers()
+  })
+
+  it('should retry a task when it fails', async () => {
+    const mocka = jest
+      .fn()
+      .mockRejectedValueOnce('ERROR 01')
+      .mockRejectedValueOnce('ERROR 02')
+      .mockResolvedValue('SUCCESS 01')
+
+    const queue = TaskQueue<{ run: undefined }>({
+      tasks: { run: mocka },
+    })
+
+    jest.useFakeTimers()
+    queue.queueTask('run')
+    queue.run()
+
+    jest.advanceTimersToNextTimer()
+    await flushPromises()
+    expect(mocka).toHaveBeenCalledTimes(1)
+
+    jest.advanceTimersToNextTimer()
+    await flushPromises()
+    expect(mocka).toHaveBeenCalledTimes(2)
+
+    jest.advanceTimersToNextTimer()
+    await flushPromises()
+    expect(mocka).toHaveBeenCalledTimes(3)
+
+    expect(queue.getQueueItems().length).toBe(0)
+
+    jest.useRealTimers()
+  })
+
+  // @todo
+  // it('should execute one task at a time', async () => {
+  //   const mocka = jest.fn()
+  //   const queue = TaskQueue<{ run1: undefined; run2: undefined }>({
+  //     tasks: {
+  //       run1: mocka,
+  //       run2: () => new Promise(() => {}),
+  //     },
+  //   })
+  //
+  //   jest.useFakeTimers()
+  //   queue.queueTask('run2')
+  //   queue.queueTask('run1')
+  //
+  //   queue.run()
+  //
+  //   jest.advanceTimersToNextTimer()
+  //   await flushPromises()
+  //
+  //   expect(mocka).not.toHaveBeenCalled()
+  //   jest.useRealTimers()
+  // })
+})
--- src/TaskQueue.ts
+++ src/TaskQueue.ts
@@ -1,167 +1,167 @@
-import { createNanoEvents } from "nanoevents";
+import { createNanoEvents } from 'nanoevents'
 
-const sleep = (timerle: number) =>
-  new Promise<void>((res) => setTimeout(res, timerle));
+const sleep = (delay: number) =>
+  new Promise<void>((res) => setTimeout(res, delay))
 
 const uuidv4 = () =>
-  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-  });
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
 
 export interface Task<G, T extends keyof G = any> {
-  id: string;
-  name: T;
-  options: any;
-  retries: number;
-  createdAt: number;
+  id: string
+  name: T
+  options: any
+  retries: number
+  createdAt: number
 }
 
-export type QueueType<G> = Task<G, keyof G>[];
+export type QueueType<G> = Task<G, keyof G>[]
 
 type Taskmap<I> = {
   [K in keyof I]: (
     options: I[K],
-    info: { retries: number; createdAt: number },
-  ) => Promise<void> | void;
-};
+    info: { retries: number; createdAt: number }
+  ) => Promise<void> | void
+}
 
 class Queue<G> {
-  public tasks: G[] = [];
+  public tasks: G[] = []
 
   constructor(elements: G[]) {
-    this.tasks = elements;
+    this.tasks = elements
   }
 
   enqueue = (e: G) => {
-    this.tasks = [...this.tasks, e];
-  };
+    this.tasks = [...this.tasks, e]
+  }
   dequeue = () => {
-    const task = this.peek();
-    this.tasks = this.tasks.slice(1);
-    return task;
-  };
+    const task = this.peek()
+    this.tasks = this.tasks.slice(1)
+    return task
+  }
   isEmpty = () => {
-    return this.tasks.length === 0;
-  };
+    return this.tasks.length === 0
+  }
   peek = () => {
-    return !this.isEmpty() ? this.tasks[0] : undefined;
-  };
+    return !this.isEmpty() ? this.tasks[0] : undefined
+  }
   length = () => {
-    return this.tasks.length;
-  };
+    return this.tasks.length
+  }
 }
 
 type QueueEvents<G> = {
-  success: (task: Task<G>) => void;
-  fail: (task: Task<G>, error: any) => void;
+  success: (task: Task<G>) => void
+  fail: (task: Task<G>, error: any) => void
   change: ({
     tasks,
     remainingTasks,
   }: {
-    tasks: Task<G>[];
-    remainingTasks: number;
-  }) => void;
-};
-type Primitives = string | number | boolean | null;
+    tasks: Task<G>[]
+    remainingTasks: number
+  }) => void
+}
+type Primitives = string | number | boolean | null
 
 export const defaultCalculateBackoff = (retryCount: number) => {
-  const noise = Math.random() * 100;
-  return Math.min(retryCount * 1000, 5000) + noise;
-};
+  const noise = Math.random() * 100
+  return Math.min(retryCount * 1000, 5000) + noise
+}
 
 export const TaskQueue = <
   G extends Record<string, O | undefined>,
-  O extends Record<string, Primitives> = any,
+  O extends Record<string, Primitives> = any
 >({
   initialState = [],
   tasks,
   onChange,
   retryDelay = defaultCalculateBackoff,
 }: {
-  initialState?: QueueType<G>;
-  tasks: Taskmap<G>;
-  onChange?: (queue: QueueType<G>) => Promise<void>;
-  retryDelay?: (retries: number) => number;
+  initialState?: QueueType<G>
+  tasks: Taskmap<G>
+  onChange?: (queue: QueueType<G>) => Promise<void>
+  retryDelay?: (retries: number) => number
 }) => {
-  const events = createNanoEvents<QueueEvents<G>>();
+  const events = createNanoEvents<QueueEvents<G>>()
 
-  const queuedTasks = new Queue(initialState);
+  const queuedTasks = new Queue(initialState)
 
-  let state: "paused" | "running" | "idle" = "paused";
+  let state: 'paused' | 'running' | 'idle' = 'paused'
 
   const queueTask = <K extends keyof G>(
     name: K,
     ...options: G[K] extends object ? [G[K]] : [undefined?]
   ) => {
-    const id = uuidv4();
+    const id = uuidv4()
     queuedTasks.enqueue({
       id,
       name,
       retries: 0,
       options: options[0],
       createdAt: Date.now(),
-    });
+    })
 
-    if (state === "idle") {
-      run();
+    if (state === 'idle') {
+      run()
     }
 
     const task = new Promise<void>((res, rej) => {
-      events.on("success", (task) => {
+      events.on('success', (task) => {
         if (task.id === id) {
-          res();
+          res()
         }
-        events.on("fail", (task, error) => {
+        events.on('fail', (task, error) => {
           if (task.id === id) {
-            rej(error);
+            rej(error)
           }
-        });
-      });
-    });
-    return { id, task };
-  };
+        })
+      })
+    })
+    return { id, task }
+  }
 
   const pause = () => {
-    state = "paused";
-  };
+    state = 'paused'
+  }
 
-  const isPaused = () => state === "paused";
+  const isPaused = () => state === 'paused'
 
   const run = async () => {
-    state = "running";
+    state = 'running'
 
     while (!queuedTasks.isEmpty()) {
       if (isPaused()) {
-        break;
+        break
       }
 
-      const task = queuedTasks.peek();
+      const task = queuedTasks.peek()
       if (!task) {
-        break;
+        break
       }
       try {
         await tasks[task.name](task.options, {
           createdAt: task.createdAt,
           retries: task.retries,
-        });
-        queuedTasks.dequeue();
-        onChange?.(queuedTasks.tasks);
-        events.emit("success", task);
+        })
+        queuedTasks.dequeue()
+        onChange?.(queuedTasks.tasks)
+        events.emit('success', task)
       } catch (e) {
-        events.emit("fail", task, e);
+        events.emit('fail', task, e)
 
-        await sleep(retryDelay(++task.retries));
+        await sleep(retryDelay(++task.retries))
       } finally {
-        events.emit("change", {
+        events.emit('change', {
           tasks: [...queuedTasks.tasks],
           remainingTasks: queuedTasks.length(),
-        });
+        })
       }
     }
 
-    state = "idle";
-  };
+    state = 'idle'
+  }
 
   return {
     run,
@@ -170,8 +170,8 @@ export const TaskQueue = <
     getQueueItems: () => [...queuedTasks.tasks],
     addEventListener: <E extends keyof QueueEvents<G>>(
       event: E,
-      callback: QueueEvents<G>[E],
+      callback: QueueEvents<G>[E]
     ) => events.on(event, callback),
     queueTask,
-  };
-};
+  }
+}
